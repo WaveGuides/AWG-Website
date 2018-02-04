b78483c25cdbba56f4f35016e1aae474547bbbcd
Bruno Herfst <hello@brunoherfst.com>
2018-02-04 15:56:14 +1100

    force push to subtree

--- _site/_publish_site.sh
+++ _site/_publish_site.sh
@@ -10,7 +10,10 @@ sh ./_build_site.sh
 # Check if working directory is clean
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
-  git subtree push --prefix _site origin gh-pages
+  git checkout master
+  git subtree split --prefix _site -b gh-pages
+  git push -f origin gh-pages:gh-pages
+  git branch -D gh-pages
   echo '----------------------------------------'
   echo 'Website updated!'
   echo '----------------------------------------'
