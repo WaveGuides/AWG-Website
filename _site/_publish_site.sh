5785ee1fc413fab71609dbdff3704b4db9f140f7
Bruno Herfst <hello@brunoherfst.com>
2018-02-04 15:34:39 +1100

    Update publish script

--- _site/_publish_site.sh
+++ _site/_publish_site.sh
@@ -7,19 +7,17 @@ cd $THISFOLDER
 # Rebuild site making sure there are no changes
 sh ./_build_site.sh
 
-if git checkout master &&
-    git fetch origin master &&
-    [ `git rev-list HEAD...origin/master --count` != 0 ] &&
-    git merge origin/master
-then
-    git subtree push --prefix _site origin gh-pages
-    echo '----------------------------------------'
-    echo 'Website updated!'
-    echo '----------------------------------------'
-else
-    echo '----------------------------------------'
-    echo 'Could not update website'
-    echo '----------------------------------------'
-    echo 'Please commit any changes to master first, before pushing live.'
+# Check if working directory is clean
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  # Working directory clean
+  git subtree push --prefix _site origin gh-pages
+  echo '----------------------------------------'
+  echo 'Website updated!'
+  echo '----------------------------------------'
+else 
+  # Uncommitted changes
+  echo '----------------------------------------'
+  echo 'Could not update website'
+  echo '----------------------------------------'
+  echo 'Please commit any changes to master first, before pushing live.'
 fi
-
